a360ea34c08b727d18cde36a22221de52258a98d
zuhaili <19004781@myrp.edu.sg>
2020-05-26 22:51:24 +0800

    UpdatedProject

--- app/src/main/java/sg/edu/rp/c346/id19004781/mymodules/ModuleDetailActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19004781/mymodules/ModuleDetailActivity.java
@@ -4,23 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ModuleDetailActivity extends AppCompatActivity {
 
     TextView tvAnswer;
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module_detail);
 
-//        tvCode = findViewById(R.id.textViewModuleCode);
-//        tvmoduleName = findViewById(R.id.textViewModulename);
-//        tvAcademicYear = findViewById(R.id.textViewAcademicYear);
-//        tvSemester = findViewById(R.id.textViewSemester);
-//        tvModuleCredit = findViewById(R.id.textViewModuleCredit);
-//        tvVenue = findViewById(R.id.textViewVenue);
         tvAnswer = findViewById(R.id.textView);
+        btnBack = findViewById(R.id.buttonBack);
 
         Intent intentReceived = getIntent();
         String code = intentReceived.getStringExtra("code");
@@ -30,6 +28,15 @@ public class ModuleDetailActivity extends AppCompatActivity {
         int moduleCredit = intentReceived.getIntExtra("moduleCredit", 4);
         String venue = intentReceived.getStringExtra("venue");
         tvAnswer.setText("Module Code: " + code + "\n Module Name: " + name + "\n Academic Year: " + year + "\n Semester: " + semester + "\n Module Credit: " + moduleCredit + "\n Venue: " + venue);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ModuleDetailActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
 
